901ed5b3dbac240432d3274443a9618826667575
Fabiola Carrizo <carrizofabiola12@gmail.com>
2022-08-03 21:53:27 -0300

    local storage y carrito

--- js/app.js
+++ js/app.js
@@ -1,35 +1,90 @@
-import { productos } from "./index.js";
-import { carritoProductos} from "./carrito.js";
+import { stockProductos } from "./stock.js";
 
 
-const mostrarProductos= (productos) => { 
+const contenedorProductos = document.getElementById("contenedor-productos")
 
-const contenedorProdutos= document.getElementById("producto_contenedor");
+const contenedorCarrito = document.getElementById('carrito-contenedor')
 
-productos.forEach( productos => {
+const botonVaciar = document.getElementById("vaciar-carrito")
+
+const precioTotal= document.getElementById('precioTotal')
+let carrito = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
+
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
+
+
+stockProductos.forEach((producto) => {
 
 const div = document.createElement("div")
-div.classList.add("card")
-div.innerHTML += `<div class="card" style="width: 18rem;">
-<img src="${productos.img}" class="card-img-top" alt="...">
-<div class="card-body"><h5 class="card-title">${productos.nombre}</h5>
-<p class="card-text">Talle: ${productos.talle}</p>
-<p class="card-text">Color: ${productos.color}</p>
-<p class="card-text">Precio:$ ${productos.precio}</p>
-<button class="boton_comprar" id=boton${productos.id}>Comprar</button></div>
+div.classList.add("producto")
+div.innerHTML= `
+<div style="width: 18rem;">
+<img src="${producto.img}" class="card-img-top" alt="...">
+<div> 
+<h5 class="card-title">${producto.nombre}</h5>
+<p> Talle: ${producto.talle}</p>
+<p> Color: ${producto.color}</p>
+<p> Precio:$ ${producto.precio}</p>
+<button class="boton_comprar" id=botonAgregar${producto.id}>Comprar</button></div>
 </div>
 `
-contenedorProdutos.appendChild(div)
 
-const boton= document.getElementById(`boton ${productos.id}`);
+contenedorProductos.appendChild(div)
 
-boton.addEventListener("click", () => {
-    carritoProductos(producto.id)
-    alert(`Se agrego ${productos.nombre} con un valor de ${productos.precio}`)
+const boton = document.getElementById(`botonAgregar${producto.id}`)
+
+boton.addEventListener('click' , () => {    
+    agregarAlCarrito(producto.id)
 })
 
 })
 
+const agregarAlCarrito = (prodId) => {
+    const item= stockProductos.find ((prod) => prod.id === prodId)
+    carrito.push(item)
+    actualizarCarrito()
+    console.log(carrito);
+} 
+
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice, 1)
+    actualizarCarrito()
+}  
+
+
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML= " "
+
+    carrito.forEach((prod) => {
+        const div = document.createElement("div")
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio:$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+        contenedorCarrito.appendChild(div)
+
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+        
+    })
+
+    contadorCarrito.innerText = carrito.length
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0)
+
+   
 }
-
-mostrarProductos(productos)
---
+++ js/stock.js
@@ -0,0 +1,11 @@
+
+
+let stockProductos =[
+    {img: '../images/ajuares.png',nombre: "Ajuar", talle: 2 , color: "blanco", precio: 1000, id:1, cantidad: 1},
+    {img: '../images/ajuares.png' ,nombre:"Ajuar" , talle: 1, color:"rosa" , precio:1000, id:2, cantidad: 1 },
+    {img: '../images/baberos.png' ,nombre:"Babero", talle: "Sin talla", color: "Azul", precio:250, id:3, cantidad: 1 },
+    {img:'../images/medias.png' ,nombre:"Medias" , talle: "sin talle", color:"Varios" , precio:250, id:4, cantidad: 1},
+];
+
+
+export {stockProductos};
